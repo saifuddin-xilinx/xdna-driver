461c5e108c4684cca2b889b899d1b58dacc5ce8d
Saifuddin Kaijar <saifuddin.kaijar@amd.com>
2026-02-13 23:30:12 +0530

    Updated few files
    Signed-off-by: Saifuddin Kaijar <saifuddin.kaijar@amd.com>

--- src/driver/amdxdna/amdxdna_drm.c
+++ src/driver/amdxdna/amdxdna_drm.c
@@ -14,9 +14,8 @@
 #include "amdxdna_devel.h"
 #endif
 
-/* Trace points disabled for prototype build */
-/* #define CREATE_TRACE_POINTS */
-/* #include "amdxdna_trace.h" */
+#define CREATE_TRACE_POINTS
+#include "amdxdna_trace.h"
 
 static int amdxdna_drm_open(struct drm_device *ddev, struct drm_file *filp)
 {
