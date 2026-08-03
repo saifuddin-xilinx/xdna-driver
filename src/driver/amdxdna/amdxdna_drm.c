5765bc342d88862405d220279a628ae52d1d0fa7
Saifuddin Kaijar <54270703+saifuddin-xilinx@users.noreply.github.com>
2026-08-03 16:55:45 +0530

    Revert: prepare for single-commit squash

--- src/driver/amdxdna/amdxdna_drm.c
+++ src/driver/amdxdna/amdxdna_drm.c
@@ -30,7 +30,6 @@ static int amdxdna_drm_open(struct drm_device *ddev, struct drm_file *filp)
 	client->pid = pid_nr(filp->pid);
 	client->uid = current_euid();
 	client->xdna = xdna;
-	kref_init(&client->refcnt);
 
 #ifdef AMDXDNA_DEVEL
 	if (iommu_mode != AMDXDNA_IOMMU_PASID)
@@ -82,19 +81,6 @@ failed:
 	return ret;
 }
 
-static void amdxdna_client_release(struct kref *ref)
-{
-	struct amdxdna_client *client =
-		container_of(ref, struct amdxdna_client, refcnt);
-
-	kfree(client);
-}
-
-void amdxdna_client_put(struct amdxdna_client *client)
-{
-	kref_put(&client->refcnt, amdxdna_client_release);
-}
-
 static void amdxdna_drm_close(struct drm_device *ddev, struct drm_file *filp)
 {
 	struct amdxdna_client *client = filp->driver_priv;
@@ -121,8 +107,22 @@ skip_sva_unbind:
 #endif
 
 	XDNA_DBG(xdna, "PID %d closed", client->pid);
-	amdxdna_client_put(client);
+	kfree(client);
 }
+
+static int amdxdna_flush(struct file *f, fl_owner_t id)
+{
+	struct drm_file *filp = f->private_data;
+	struct amdxdna_client *client = filp->driver_priv;
+	struct amdxdna_dev *xdna = client->xdna;
+	pid_t pid = task_tgid_nr(current);
+	int idx;
+
+	/* When current PID not equals to Client PID, this is a flush()
+	 * triggered by closing a child process. If this is the case, flush() is
+	 * just a no-op. The process which open() device should finally flush()
+	 * and close() device.
+	 */
 	if (pid != client->pid)
 		return 0;
 
